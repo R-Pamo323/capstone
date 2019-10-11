fc6ea030e88d6fd68b5947cf56f33a944e58088f
R-Pamo323 <33794424+R-Pamo323@users.noreply.github.com>
2019-10-11 10:45:15 -0500

    Hecho con la tranferencia acabada

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -54,6 +54,8 @@ class PagesController extends Controller
         $pacienteNuevo->posta_id = $request->posta_id;
         $pacienteNuevo->save();
 
+
+        ////////////////// Se deberia Borrar /////////////////
         $dni = $request->get('dni'); //se obtiene el nombre del .blade
         $apellido = $request->get('apellido');
         $nombre = $request->get('nombre');
@@ -69,24 +71,41 @@ class PagesController extends Controller
 
     public function datostransferencia(Request $request){
         //return $request->all();
-        $pacienteNuevo = new Paciente;
-        $pacienteNuevo->pac_nombre = $request->pac_nombre;
-        $pacienteNuevo->pac_apellido= $request->pac_apellido;
-        $pacienteNuevo->pac_dni = $request->pac_dni;
-        $pacienteNuevo->pac_sexo = $request->pac_sexo;
-        $pacienteNuevo->pac_email = $request->pac_email;
-        $pacienteNuevo->pac_nacimiento = $request->pac_nacimiento;
-        $pacienteNuevo->pac_pais= $request->pac_pais;
-        $pacienteNuevo->pac_departamento = $request->pac_departamento;
-        $pacienteNuevo->pac_provincia = $request->pac_provincia;
-        $pacienteNuevo->pac_distrito = $request->pac_distrito;
-        $pacienteNuevo->pac_direccion= $request->pac_direccion;
-        $pacienteNuevo->pac_asegurado = $request->pac_asegurado;
-        $pacienteNuevo->pac_estcivil = $request->pac_estcivil;
-        $pacienteNuevo->pac_estudio = $request->pac_estudio;
-        $pacienteNuevo->pac_seguro = $request->pac_seguro;
-        $pacienteNuevo->posta_id = $request->posta_id;
-        $pacienteNuevo->save();
+        $transferenciaNuevo = new Traslado;
+        $transferenciaNuevo->tra_fecha = $request->tra_fecha;
+        $transferenciaNuevo->tra_anamnesis= $request->tra_anamnesis;
+        $transferenciaNuevo->tra_diagnostico = $request->tra_diagnostico;
+        $transferenciaNuevo->tra_tratamiento = $request->tra_tratamiento;
+        $transferenciaNuevo->tra_coordinacion = $request->tra_coordinacion;
+        $transferenciaNuevo->tra_condicion = $request->tra_condicion;
+        $transferenciaNuevo->tra_especialidad= $request->tra_especialidad;
+        $transferenciaNuevo->tra_persona_acompanante= $request->tra_persona_acompanante;
+        $transferenciaNuevo->tra_o_presionarterial = $request->tra_o_presionarterial;
+        $transferenciaNuevo->tra_o_respiracion = $request->tra_o_respiracion;
+        $transferenciaNuevo->tra_o_pulso = $request->tra_o_pulso;
+        $transferenciaNuevo->tra_o_temperatura= $request->tra_o_temperatura;
+        $transferenciaNuevo->tra_a_recomendacion = $request->tra_a_recomendacion;
+        $transferenciaNuevo->tra_a_problemas_esperados = $request->tra_a_problemas_esperados;
+        $transferenciaNuevo->tra_confirmacion = $request->tra_confirmacion;
+        $transferenciaNuevo->clinica_id = $request->clinica_id;
+        $transferenciaNuevo->posta_id = $request->posta_id;
+        $transferenciaNuevo->paciente_id = $request->paciente_id;
+        $transferenciaNuevo->user_id = $request->user_id;
+
+        $transferenciaNuevo->save();
+
+        ////////////////// Se deberia Borrar /////////////////
+        $dni = $request->get('dni'); //se obtiene el nombre del .blade
+        $apellido = $request->get('apellido');
+        $nombre = $request->get('nombre');
+         
+        $paciente = Paciente::orderBy('id', 'ASC')
+                     ->nombre($nombre)  //sin $ son las variables que iran en nuestro PACIENTE.PHP
+                     ->apellido($apellido)
+                     ->dni($dni)
+                     ->paginate(4);
+        return view('ubuscar', compact('paciente'));
+
     }
 
 
--- resources/views/create/createtransferencia.blade.php
+++ resources/views/create/createtransferencia.blade.php
@@ -9,7 +9,7 @@
         @csrf
         <div>Datos Medico<br>
             @foreach($doctor as $user)
-                <input type="hidden" name="doc_id" value={{$user->id}}> 
+                <input type="hidden" name="user_id" value={{$user->id}}> 
                 <input type="text" name="doc_nombre" placeholder="Nombre Medico..." class="form-control mb-2" value ={{$user->doc_nombre}} >
                 <br>
                 <input type="text" name="doc_especialidad" placeholder="Especialidad..." class="form-control mb-2" value ={{$user->doc_especialidad}}>
@@ -19,6 +19,7 @@
         </div>
         <div>Datos Paciente <br>
             @foreach($paciente as $user1)
+                <input type="hidden" name="paciente_id" value={{$user1->id}}> 
                 <input type="text" name="pac_nombre" placeholder="Nombre..." class="form-control mb-2" value ={{$user1->pac_nombre}}>
                 <br>
                 <input type="text" name="pac_apellido" placeholder="Apellido..." class="form-control mb-2" value ={{$user1->pac_apellido}}>
@@ -35,21 +36,59 @@
             
         </div>
         <div>
-            Otros <br>
-            <select name="pac_estudio">
-                <option>Primarios</option>
-                <option>Secundarios</option>
-                <option selected>Superior</option>
-                <option>Universitario</option>
-            </select>
-            <select name="pac_estcivil">
-                <option selected>Soltero/a</option>
-                <option>Casado/a</option>
-                <option>Divorciado/a</option>
-                <option>Viudo/a</option>
-            </select>
+            Datos Generales <br>
+                <input type="date" name="tra_fecha" placeholder="Fecha..." class="form-control mb-2">
+                <br>
+                <input type="number" name="posta_id" placeholder="Origen..." class="form-control mb-2">
+                <br>
+                <input type="number" name="clinica_id" placeholder="Destino..." class="form-control mb-2">
+                <br>
         </div>
-        <button class = "btn btn-primary btn-block" type="submit">Guardar</button> <button>Cancelar</button>
+        <div>Resumen Historia Clinica
+                <input type="text" name="tra_anamnesis" placeholder="Anamnesis..." class="form-control mb-2">
+                <br>
+                <input type="text" name="tra_diagnostico" placeholder="Diagnostico..." class="form-control mb-2">
+                <br>
+                <input type="text" name="tra_tratamiento" placeholder="Tratamiento..." class="form-control mb-2">
+                <br>
+                <input type="text" name="tra_coordinacion" placeholder="Coordinacion..." class="form-control mb-2">
+                <br>
+                <input type="number" name="tra_condicion" placeholder="Condicion..." class="form-control mb-2">
+                <br>
+                <input type="text" name="tra_especialidad" placeholder="Especialidad..." class="form-control mb-2">
+                <br>
+                <input type="text" name="tra_persona_acompanante" placeholder="Acompañante..." class="form-control mb-2">
+                <br>
+        </div>
+        <div class="form-group row"> Observaciones <br>
+                <div class="col-xs-2">
+                  <label for="ex1">Presion Arterial</label>
+                  <input class="form-control" id="ex1" type="number" step="0.01" name="tra_o_presionarterial">
+                </div>
+                <div class="col-xs-2">
+                  <label for="ex2">Respiracion</label>
+                  <input class="form-control" id="ex2" type="number" step="0.01" name="tra_o_respiracion">
+                </div>
+                <div class="col-xs-2">
+                  <label for="ex3">Pulso</label>
+                  <input class="form-control" id="ex3" type="number" step="0.01" name="tra_o_pulso">
+                </div>
+                <div class="col-xs-2">
+                    <label for="ex4">Temperatura</label>
+                    <input class="form-control" id="ex4" type="number" step="0.01" name="tra_o_temperatura">
+                </div>
+        </div>
+        <div>Recomendaciones
+                <input type="text" name="tra_a_recomendacion" placeholder="Recomendacion..." class="form-control mb-2">
+                <br>
+                <input type="text" name="tra_a_problemas_esperados" placeholder="Problemas Esperados..." class="form-control mb-2">
+                <br>
+                <input type="number" name="tra_confirmacion" placeholder="Confirmacion..." class="form-control mb-2">
+                <br>
+        </div>
+                
+        
+        <button class = "btn btn-primary btn-block" type="submit">Guardar</button> 
     </form>
 
 
